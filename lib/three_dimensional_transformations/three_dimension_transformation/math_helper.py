826baee775ce8700301ed316c211457794a09aa7
4dreadhead <slava14042000@mail.ru>
2022-03-25 14:18:46 +0700

    Some fixes

--- lib/three_dimensional_transformations/three_dimension_transformation/math_helper.py
+++ lib/three_dimensional_transformations/three_dimension_transformation/math_helper.py
@@ -2,9 +2,10 @@ from numba import njit
 
 
 class MathHelper:
+    # Fast math calculating determinant
     @staticmethod
     @njit(fastmath=True)
-    def determinant(matrix_0: tuple, matrix_1: tuple, matrix_2: tuple) -> int:
+    def determinant(matrix_0, matrix_1, matrix_2):
         result = matrix_0[0] * matrix_1[1] * matrix_2[2] \
                  - matrix_0[0] * matrix_1[2] * matrix_2[1] \
                  - matrix_0[1] * matrix_1[0] * matrix_2[2] \
@@ -13,12 +14,14 @@ class MathHelper:
                  - matrix_0[2] * matrix_1[1] * matrix_2[0]
         return result
 
+    # Fast math calculating depth for z-buffer
     @staticmethod
     @njit(fastmath=True)
     def z_calculate(x, y, determinant_a, determinant_b, determinant_c, determinant_d):
         return (determinant_b * y - determinant_a * x + determinant_d) / \
                determinant_c if determinant_c != 0 else (determinant_b * y - determinant_a * x + determinant_d)
 
+    # Prepare and calculate depth for z-buffer
     @staticmethod
     def z_frequency(apex_a, apex_b, apex_c, x, y):
         determinant_a = MathHelper.determinant((apex_a[1], apex_a[2], 1),
@@ -42,7 +45,7 @@ class MathHelper:
 
     # Sort apexes of triangle by 'y' coordinates
     @staticmethod
-    def sort_apexes(triangle, ascending=False, descending=False):
+    def sort_apexes(triangle, descending=False):
         for _ in range(3):
             for index in range(1, 3):
                 if descending:
@@ -54,5 +57,3 @@ class MathHelper:
     @staticmethod
     def set_color_step(polygon, z_max, z_color_step):
         return round((z_max - polygon[0][2]) / z_color_step) - int((z_max/z_color_step) / 10)
-
-
